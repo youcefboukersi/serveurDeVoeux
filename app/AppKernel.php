cac5ce5204af1d6790ddb76944e08598bcb778f7
Benkhellat Omar <omarben07@outlook.fr>
2016-12-20 13:56:52 +0100

    2em commit

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,8 +19,6 @@ class AppKernel extends Kernel
             new SRVDV\ServerBundle\SRVDVServerBundle(),
 
             new FOS\UserBundle\FOSUserBundle(),
-
-
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- src/SRVDV/ServerBundle/SRVDVServerBundle.php
+++ src/SRVDV/ServerBundle/SRVDVServerBundle.php
@@ -6,12 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SRVDVServerBundle extends Bundle
 {
-<<<<<<< HEAD
-=======
+
 
     public function getParent()
     {
         return 'FOSUserBundle';
     }
->>>>>>> c42f38fdc8f038834e4adf77a1d6da15dfccc51c
+
 }
